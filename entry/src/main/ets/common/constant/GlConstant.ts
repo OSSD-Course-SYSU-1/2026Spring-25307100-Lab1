c99df5b8e1282dd009a544c8ffd3034c04368123
yangyongzhen <534117529@qq.com>
2024-11-23 23:20:48 +0800

    增加首选项封装

---
+++ entry/src/main/ets/common/constant/GlConstant.ts
@@ -0,0 +1,33 @@
+export class GlConstant {
+  // 日志
+  static readonly LOG_DOMAIN: number = 0x5467;
+  static readonly LOG_TAG: string = "FHLog";
+
+  // 页面尺寸
+  static readonly PAGE_FULL: string = "100%";
+  static readonly PRE_96: string = "96%";
+  static readonly PRE_90: string = "90%";
+  static readonly PRE_35: string = "35%";
+  static readonly PRE_30: string = "30%";
+
+  static readonly SIZE_128: number = 128;
+  static readonly SIZE_108: number = 108;
+  static readonly SIZE_81: number = 81;
+
+  static readonly SIZE_54: number = 54;
+  static readonly SIZE_48: number = 48;
+  static readonly SIZE_32: number = 32;
+  static readonly SIZE_28: number = 28;
+  static readonly SIZE_24: number = 24;
+  static readonly SIZE_18: number = 18;
+  static readonly SIZE_16: number = 16;
+  static readonly SIZE_14: number = 14;
+  static readonly SIZE_12: number = 12;
+  static readonly SIZE_8: number = 8;
+  static readonly SIZE_4: number = 4;
+
+  static readonly SHM_API_APP_ID: string = "zx22HPMFi5GakgrYwnhys7F5RQfRnwnU";
+  static readonly SHM_API_APP_SECURITY: string = "lcmOz5MDVnP1lbej7gFo7tHGmd6QxPv2";
+
+  static readonly LOAN_TERM_ARR: Array<number> = [5, 10, 15, 20, 25, 30];
+}
